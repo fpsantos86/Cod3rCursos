a00ce01c5986e548f2466ac0b96f1bfaca3b05f7
Felipe Pereira dos Santos <fpsantos86@hotmail.com>
2018-06-12 17:06:32 -0300

    Seção 5, Aula 37 - 6:08

---
+++ meat-app-starter-master/src/app/restaurants/restaurant/restaurant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurant',
+  templateUrl: './restaurant.component.html'
+})
+export class RestaurantComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html',
+  styleUrls: ['./restaurants.component.css']
+})
+export class RestaurantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
