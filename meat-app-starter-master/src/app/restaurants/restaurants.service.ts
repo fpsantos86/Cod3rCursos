cc27f0d9c5dfa49ea5cce7c969eec3af635d8221
Felipe Pereira dos Santos <fpsantos86@hotmail.com>
2018-06-14 09:36:43 -0300

    Seção 5, Aula 47 - Detalhe do Restaurant
    Não entendi muito bem, o porquê de ter que definir o objeto restaurant como se aceitasse nulo (raurant?), sendo que ele possuia todos os dados necessários a serem exibidos. No caso contrario ele não exibia os dados e apresentava erro no console.

--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -19,4 +19,11 @@ constructor (private http: Http){}
         .map(response => response.json())
         .catch(ErrorHandler.handleError)
   }
+
+  restaurantById(id: string): Observable<Restaurant>{
+
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
+  }
 }
