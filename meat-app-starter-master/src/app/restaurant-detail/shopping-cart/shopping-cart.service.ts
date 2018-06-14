e3422fb4b096578e32932bef66b9ad4464500499
Felipe Pereira dos Santos <fpsantos86@hotmail.com>
2018-06-14 17:08:15 -0300

    Seção 5, Aula 53 - Aplicando Eventos no Componente do Carrinho de Compras
    - Nessa aula foi usado um recurso novo que não havia sido comentado antes: "Template Variable" , parece que através de uma referencia  com serquilha (ex.:#shoppingCart) foi possível chamar o método do componente filho no componente pai.

--- meat-app-starter-master/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ meat-app-starter-master/src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -10,9 +10,7 @@ export class ShoppingCartService{
 
   addItem(item:MenuItem){
     let foundItem = this.items.find((mItem) => mItem.menuItem.id ===item.id)
-
-    if(foundItem)
-    {
+    if(foundItem){
       foundItem.quantity = foundItem.quantity + 1
     }else{
       this.items.push(new CartItem(item))
@@ -20,7 +18,7 @@ export class ShoppingCartService{
   }
 
   removeItem(item:CartItem){
-    this.items.splice
+    this.items.splice(this.items.indexOf(item),1)
   }
 
   total():number{
