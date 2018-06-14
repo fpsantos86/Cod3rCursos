00c3949f6a8405b5802cfcd3ddfa7e3cd8404a5b
Felipe Pereira dos Santos <fpsantos86@hotmail.com>
2018-06-14 14:22:33 -0300

    Seção 5, Aula 51 - Implementando o Carrinho de Compras
     - Nessa aula foi criado o modelo do item de menu.
     - Foi criado um método na classe de serviço do restaurante para retornar uma Observable de MenuItem
     - No template, de forma parecida com o que foi feio com o componente Reviews, implementei a listagem dos Itens de Menu com o AsyncPipe dentro da "diretiva" *ngFor.
    - Conforme dito na aula, o AyncPipe automatiza o processo de inscrever-se no Observable retornado pela classe Service.

--- meat-app-starter-master/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter-master/src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import {Restaurant} from "./restaurant/restaurant.model"
 
 import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -32,4 +33,11 @@ constructor (private http: Http){}
     .map(response => response.json())
     .catch(ErrorHandler.handleError)
   }
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
+  }
+
 }
